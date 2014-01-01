eb9bbcde9895d5fb8edf43cf2727df1d872d0cf0
ani <ani@anis>
2014-01-01 18:59:23 -0430

    Ya están las validaciones de las fechas en la creación de la acción.

--- web/JS/Validacion-Accion.js
+++ web/JS/Validacion-Accion.js
@@ -6,6 +6,8 @@ accion= document.getElementById("accion");
 proceso= document.getElementById("proceso");
 responsable= document.getElementById("responsable");
 recursos= document.getElementById("recursos");
+fechainicioinput = document.getElementById("fechainicioinput");
+fechafinalinput = document.getElementById("fechafinalinput");
 submit=document.getElementById("submit");
 
 //se definen contadores de textareas
@@ -16,12 +18,15 @@ accion_error= document.getElementById("accion_error");
 proceso_error= document.getElementById("proceso_error");
 responsable_error= document.getElementById("responsable_error");
 recursos_error= document.getElementById("recursos_error");
-
+fechainicioinput_error = document.getElementById("fechainicioinput_error");
+fechafinalinput_error = document.getElementById("fechafinalinput_error");
 //se definen las variables de validacion
 accion_value= false;
 proceso_value= false;
 responsable_value= false;
 recursos_value= false;
+fechainicioinput_value = false;
+fechafinalinput_value = false;
 
 /*
  * se desactiva el boton de submit hasta que se llene correctamente el formulario
@@ -59,6 +64,8 @@ if(responsable.value=="Responsable"){
 }else{
     responsable.setAttribute("style","width:300px; color: black; font-size: 14px");
 }
+
+
 
 
 
@@ -121,6 +128,22 @@ responsable.onclick = function() {
  }
 };
 
+fechainicioinput.onclick = function() {
+     fechainicioinput.setAttribute("style", "width:300px; color: black;font-size: 14px; border-color: #127ba5");
+     fechainicioinput_error.innerHTML = "";
+ if (fechainicioinput.value == "dd-mm-yyyy") {
+     fechainicioinput.value = "";
+ }
+};
+
+fechafinalinput.onclick = function() {
+     fechafinalinput.setAttribute("style", "width:300px; color: black;font-size: 14px; border-color: #127ba5");
+     fechafinalinput_error.innerHTML = "";
+ if (fechafinalinput.value == "dd-mm-yyyy") {
+     fechafinalinput.value = "";
+ }
+};
+
 /*
  * onblur events
  * 
@@ -144,7 +167,7 @@ accion.onblur = function() {
          accion_value = true;
     }
  }
-  submit.disabled = !(accion_value && proceso_value && recursos_value && responsable_value);
+  submit.disabled = !(accion_value && proceso_value && recursos_value && responsable_value && fechafinalinput_value && fechainicioinput_value);
 };
 
 recursos.onblur = function() {
@@ -170,7 +193,7 @@ recursos.onblur = function() {
         recursos_value= true;
     }
  }
-   submit.disabled = !(accion_value && proceso_value && recursos_value && responsable_value);
+   submit.disabled = !(accion_value && proceso_value && recursos_value && responsable_value && fechafinalinput_value && fechainicioinput_value);
 };
 
 responsable.onblur = function() {
@@ -190,7 +213,7 @@ responsable.onblur = function() {
         responsable_value = true;
     }
  }
-   submit.disabled = !(accion_value && proceso_value && recursos_value && responsable_value);
+   submit.disabled = !(accion_value && proceso_value && recursos_value && responsable_value && fechafinalinput_value && fechainicioinput_value);
 };
 
 proceso.onblur = function() {
@@ -210,6 +233,47 @@ proceso.onblur = function() {
         proceso_value = true;
     }
  }
-   submit.disabled = !(accion_value && proceso_value && recursos_value && responsable_value);
+   submit.disabled = !(accion_value && proceso_value && recursos_value && fechafinalinput_value && fechainicioinput_value);
 };
 
+fechainicioinput.onblur = function() {
+    fechainicioinput.value = fechainicioinput.value.trim();
+ if (fechainicioinput.value == "") {
+     fechainicioinput.setAttribute("style", "width:300px; color: gray; font-size: 14px; border-color: #127ba5; border-color: #B22222");
+     fechainicioinput_error.innerHTML = "Indique fecha de inicio";
+     fechainicioinput.value = "dd-mm-yyyy";
+     fechainicioinput_value = false;
+ }else{
+     var dateReg = /^([0-2][0-9]|3[0-1])-(0[1-9]|1[0-2])-\d{4}$/;
+     if (!dateReg.test(fechainicioinput.value)){
+         fechainicioinput.setAttribute("style", "width:300px; color: black; font-size: 14px; border-color: #127ba5; border-color: #B22222");
+         fechainicioinput_error.innerHTML = "Siga el formato preestablecido 'dd-mm-yyyy'";
+         fechainicioinput_value = false;
+     }else{
+        fechainicioinput.setAttribute("style", "width:300px; color: black; font-size: 14px");
+        fechainicioinput_value = true;
+    }
+ }
+   submit.disabled = !(accion_value && proceso_value && recursos_value && fechafinalinput_value && fechainicioinput_value);
+};
+
+fechafinalinput.onblur = function() {
+    fechafinalinput.value = fechafinalinput.value.trim();
+ if (fechafinalinput.value == "") {
+     fechafinalinput.setAttribute("style", "width:300px; color: gray; font-size: 14px; border-color: #127ba5; border-color: #B22222");
+     fechafinalinput_error.innerHTML = "Indique fecha de finalización";
+     fechafinalinput.value = "dd-mm-yyyy";
+     fechafinalinput_value = false;
+ }else{
+     var dateReg = /^([0-2][0-9]|3[0-1])-(0[1-9]|1[0-2])-\d{4}$/;
+     if (!dateReg.test(fechafinalinput.value)){
+         fechafinalinput.setAttribute("style", "width:300px; color: black; font-size: 14px; border-color: #127ba5; border-color: #B22222");
+         fechafinalinput_error.innerHTML = "Siga el formato preestablecido 'dd-mm-yyyy'";
+         fechafinalinput_value = false;
+     }else{
+        fechafinalinput.setAttribute("style", "width:300px; color: black; font-size: 14px");
+        fechafinalinput_value = true;
+    }
+ }
+   submit.disabled = !(accion_value && proceso_value && recursos_value && fechafinalinput_value && fechainicioinput_value);
+};
